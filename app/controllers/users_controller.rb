0edab0cc0b25fb6727e72932d6ebddd4d50247bf
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-10-14 23:38:40 -0700

    created pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def index
-    @users = User.by_karma.limit(50)
+  	@page = params[:page].to_i
+  	@page = 1 if @page == 0
+    @users = User.by_karma.limit(50).offset(50*(@page-1))
+    p @page
+	case @page
+	when 2
+		@paginate = [-1, 0, 1, 2, 3]
+	when 1
+		@paginate = [0, 1, 2, 3, 4]
+	else
+		@paginate = [-2,-1,0,1,2] 
+	end
+	p "*"*1000
+	p @paginate
   end
 end
